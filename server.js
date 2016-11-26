4787b78b6c61d5f82f080b83e4a6d51eb5b75776
JackLinUWaterloo <jack.lin@hotmail.ca>
2016-11-26 14:06:08 -0500

    Adding server.js and package.json

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/hello', function(req, res) {
+  res.send('HI!');
+});
+
+app.listen(3000, function() {
+  console.log('App Started on localhost:3000');
+});
