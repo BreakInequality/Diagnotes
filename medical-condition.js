198b5b97e058d22991a1231e64626cdf966dc8e9
JackLinUWaterloo <jack.lin@hotmail.ca>
2016-11-26 21:33:51 -0500

    updated medical-condition javascript

--- medical-condition.js
+++ medical-condition.js
@@ -1,7 +1,7 @@
 var request = require("request");
 var _ = require("lodash");
 
-var options = { method: 'GET',
+var symptomsJSON = { method: 'GET',
   url: 'https://api.infermedica.com/v2/symptoms',
   headers: {
     app_key: '60419814a9f4a76540b3940de5da6384',
@@ -9,10 +9,24 @@ var options = { method: 'GET',
   }
 };
 
+var diagnosisJSON = { method: 'POST',
+  url: 'https://api.infermedica.com/v2/diagnosis',
+  headers: {
+    app_key: '60419814a9f4a76540b3940de5da6384',
+    app_id: 'dd20d4d6',
+    "content-type": "application/json"
+  },
+  body: {
+    sex:"male",
+    age:"29",
+    evidence: [ { "id": "s_21", "choice_id": "present" } ]
+  }
+};
+
 var getAllSymptoms = function(callback) {
   var symptoms = [];
 
-  request(options, function (error, response, body) {
+  request(symptomsJSON, function (error, response, body) {
     if (error) throw new Error(error);
     symptoms = _.map(JSON.parse(body), function(symptom) {
       return {"id": symptom.id, "name":symptom.name};
@@ -26,24 +40,33 @@ var getAllSymptoms = function(callback) {
   });
 };
 
-var getSymptomIds = function(symptoms, userSymptoms) {
+var getSymptomIds = function(symptoms, userSymptoms, callback) {
   userSymptoms = ["headache"];
   var symptomsArray = [];
 
   _.forEach(userSymptoms, function(userSymptom) {
     _.forEach(symptoms, function(symptom) {
-
-      if(symptom.name.indexOf(userSymptom) !== -1) {
+      if(symptom.name.toUpperCase().indexOf(userSymptom.toUpperCase()) !== -1) {
         symptomsArray.push(symptom.id);
         return false;
       }
     });
   });
-  console.log(symptomsArray);
-}
+  if(typeof callback === 'function') {
+    callback(symptoms);
+  } else {
+    throw new Error("ERROR CALLBACK FUNCTION");
+  }
+};
 
-getAllSymptoms(function(symptoms) {
-  getSymptomIds(symptoms);
+getAllSymptoms(function(symptoms, userSymptoms) {
+  getSymptomIds(symptoms, userSymptoms, function() {
+      request(diagnosisJSON, function(error, response, body) {
+        console.log("ERROR");
+        if (error) throw new Error(error);
+        console.log(body);
+      });
+  });
 });
 
 module.exports = {
