830706e4678d1745f6c180605ac2541b16e74082
Jashan S <jashan1998s@gmail.com>
2016-11-26 20:00:38 -0500

    Testing new diagnosis

--- diagnose.js
+++ diagnose.js
@@ -2,15 +2,69 @@
  * Created by Jashan Shewakramani
  * Makes diagnosis
  */
-exports.makeDiagnosis = function(phoneNumber, symptoms, db) {
-  db.collection('patients')
-      .insertOne({'phone': phoneNumber, 'symptoms': symptoms}, function(err) {
-          if (err != null) {
-              console.log('There was an error: ', err);
-          }
-  });
 
-  handleSymptoms(symptoms);
+var twilio = require('twilio');
+
+var makeDiagnosis = function(body, db) {
+    db.findOne({_id: body.From}, function(err, doc) {
+        if (err) {
+            console.log('Error while fetching number: ' + body.From);
+        } else {
+            if (doc == undefined) {
+                console.err('Unexpected unfound doc: ' + body.From);
+            } else {
+                doc['diagnosis'] = body.Body;
+                db.updateOne({_id: body.From}, {$set: {'symptoms': body.Body}});
+            }
+        }
+    })
+};
+
+exports.getResponse = function(body, db) {
+    if (body.Body === 'sign up') {
+        return new twilio.TwimlResponse().message('Sign up with your name, age and sex in this format:\n sign Jashan S, 17, M');
+    } else {
+        db.collection('patients').findOne({'phone': body.From}, function(err, doc) {
+            if (err) {
+                return new twilio.TwimlResponse().message('There was an error; please try again later');
+            } else if (!doc){
+                if (validateSignUp(body.Body)) {
+                    signUp(body, db);
+                    return new twilio.TwimlResponse('Thanks for signing up. You may now request diagnoses');
+                }
+                return new twilio.TwimlResponse().message('Ensure you properly sign up before requesting a diagnosis');
+            } else {
+                makeDiagnosis(body, db);
+                return new twilio.TwimlResponse().message('Your diagnosis is being processed. You will be contacted shortly');
+            }
+        });
+    }
+};
+
+var validateSignUp = function(text) {
+    if (text.includes('sign')) {
+        var updatedString = text.substr(5);
+        var arr = updatedString.split(',');
+        console.log('Validated on text: ' + text, arr.length == 3);
+        return arr.length == 3;
+    }
+};
+
+var signUp = function(body, db) {
+    var details = body.Body.substr(4).split(',');
+    var doc = {
+        '_id': body.From,
+        'name': details[0],
+        'age': parseInt(details[1], 10),
+        'sex': details[2]
+    };
+
+    db.insertOne(doc, function(err) {
+        if (err) {
+            console.log('Error inserting doc: ', err);
+        }
+    })
+
 };
 
 
@@ -19,6 +73,7 @@ var handleSymptoms = function(symptoms) {
     for (var i = 0; i < parsedSymptoms.length; i++) {
         parsedSymptoms[i] = parsedSymptoms[i].replace(/ /g, '');
     }
+    console.log('Parsed symptoms: ', parsedSymptoms);
 
-    
+
 };
--- server.js
+++ server.js
@@ -28,13 +28,10 @@ require('mongodb').MongoClient.connect(process.env.MONGO_URI || 'mongodb://local
 
 // Handles incoming requests from Twilio
     app.post('/sms', function(req, res) {
-      var twilio = require('twilio');
-      var twiml = new twilio.TwimlResponse();
-      twiml.message('We have received your symptoms. You will be contacted with a diagnosis shortly');
+      var twimlResponse = require('./diagnose').getResponse(req.body, db);
       res.writeHead(200, {'Content-Type': 'text/xml'});
-      res.end(twiml.toString());
-      console.log(req.body);
-      diagnose(req.body.From, req.body.Body, db);
+      res.end(twimlResponse.toString());
+
     });
     app.listen(port, function() {
       console.log('App started on localhost:%s', port);
