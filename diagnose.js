64d79c8c135e935de64451ffaeeaab1a3609f889
Jashan S <jashan1998s@gmail.com>
2016-11-26 20:32:39 -0500

    final checks

--- diagnose.js
+++ diagnose.js
@@ -31,7 +31,7 @@ exports.getResponse = function(body, db, callback) {
                 callback(twiml);
             } else if (!doc){
                 if (validateSignUp(body.Body)) {
-                    var twiml = new twilio.TwimlResponse('Thanks for signing up. You may now request diagnoses');
+                    twiml = new twilio.TwimlResponse('Thanks for signing up. You may now request diagnoses');
                     callback(twiml);
                     signUp(body, db, callback);
                 } else {
