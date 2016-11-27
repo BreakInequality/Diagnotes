9be6381e36c8378dcf217a98d492b2032335aca6
Jashan S <jashan1998s@gmail.com>
2016-11-26 20:38:10 -0500

    Finalized patient sign up; move on to diagnosis

--- diagnose.js
+++ diagnose.js
@@ -11,7 +11,7 @@ var makeDiagnosis = function(body, db) {
 
 exports.getResponse = function(body, db, callback) {
     var twiml;
-    if (body.Body === 'sign up') {
+    if (body.Body.toLowerCase().trim() === 'sign up') {
         return  callback(twilio.TwimlResponse().message('Sign up with your name, age and sex in this format:\n sign Jashan S, 17, M'));
     } else {
         db.collection('patients').findOne({'phone': body.From}, function(err, doc) {
